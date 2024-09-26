b00fb66c36ff08d158662a2b8786357bb843f762
AOmwenga <aomwenga@gmail.com>
2024-09-26 14:25:23 -0500

    Cached

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// Cache at least one element using selectElementById.
+let entireSection = document.getElementById(`entire-section`);
+console.log(entireSection);
+// Cache at least one element using querySelector or querySelectorAll.
+let factsSection = entireSection.querySelector(`h2`);
+// console.log(factsSection);
