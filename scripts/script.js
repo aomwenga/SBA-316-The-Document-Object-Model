03d1f47c7bc04d3ac2bac6beec4ef2bb80f1af53
AOmwenga <aomwenga@gmail.com>
2024-09-26 15:43:41 -0500

    Cloning

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,31 @@
 // Cache at least one element using selectElementById.
 let entireSection = document.getElementById(`entire-section`);
-console.log(entireSection);
+// console.log(entireSection);
+
 // Cache at least one element using querySelector or querySelectorAll.
-let factsSection = entireSection.querySelector(`h2`);
-// console.log(factsSection);
+let ul = entireSection.querySelector(`ul`);
+// console.log(ul);
+
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+let beyondBasketball = ul.lastElementChild;
+beyondBasketball.textContent = beyondBasketball.textContent.toUpperCase();
+console.log(beyondBasketball);
+
+// Iterate over a collection of elements to accomplish some task.
+let li = ul.querySelectorAll(`li`);
+li.forEach((i) => {
+  i.textContent = i.textContent.toUpperCase();
+});
+
+// Create at least one element using createElement.
+let myFavStat = document.createElement(`li`);
+myFavStat.textContent = `Has to be the fact he won 2 more rings after Shaq`;
+
+// Use appendChild and/or prepend to add new elements to the DOM.
+ul.appendChild(myFavStat);
+
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content.
+let clone = document.querySelector(`li`);
+let clonedLi = clone.cloneNode(true);
+
+// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
